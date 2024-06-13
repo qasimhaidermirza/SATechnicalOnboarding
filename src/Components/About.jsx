705c1f19c585607afe42c23018e0caa21368315d
Qasim Haider <123164437+qasimhaidermirza@users.noreply.github.com>
2024-06-13 11:40:56 +0000

    commit changed

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,29 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/about 2.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "abstract background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a persuade of Bachellor's of Science in the field of Artificial Intelligence exploring the domains of data science and AI.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Programming (C++ & Python)",
+  "Data Handling & Manipulation",
+  "Data Analysis",
+  "Machine Learning",
+  "Problem Solving",
+  "Coordination & Teamwork",
+  "Content Writing",
+  "Graphic Designing",
 ];
 
 /**
@@ -45,7 +47,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about working in the field and learning and expanding my knowledge via practical applications.";
 
 const About = () => {
   return (
